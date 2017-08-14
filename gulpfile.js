1773129284bc0563d357d7aa8ee56ed2e04ec621
Gurucharan94 <guru11294@gmail.com>
2017-08-14 23:45:24 +0530

    Coverted to a portfolio

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,20 @@ var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
+var concat = require('gulp-concat');
+
 
 // Set the banner content
 var banner = ['/*!\n',
     ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
     ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
+    ' * Licensed under MIT License\n',
     ' */\n',
     ''
 ].join('');
 
-// Compile LESS files from /less into /css
+// Compile LESS files from /less into /css .Note that creative.less imports the other
+// 2 files. 
 gulp.task('less', function() {
     return gulp.src('less/creative.less')
         .pipe(less())
@@ -27,22 +30,59 @@ gulp.task('less', function() {
         }))
 });
 
-// Minify compiled CSS
-gulp.task('minify-css', ['less'], function() {
-    return gulp.src('css/creative.css')
+gulp.task('copy-css',['less'],function(){
+    
+   return gulp.src([
+            'node_modules/font-awesome/fonts/*',
+            '!node_modules/font-awesome/**/*.map',
+            '!node_modules/font-awesome/.npmignore',
+            '!node_modules/font-awesome/*.txt',
+            '!node_modules/font-awesome/*.md',
+            '!node_modules/font-awesome/*.json'
+        ])
+        .pipe(gulp.dest('./dist/fonts'))
+        .pipe(
+             gulp.src(['node_modules/bootstrap/dist/css/bootstrap.css',
+              'node_modules/font-awesome/css/font-awesome.css',
+            'node_modules/magnific-popup/dist/magnific-popup.css',
+        ])
+            .pipe(gulp.dest('css'))
+            .pipe(browserSync.reload({
+                stream: true
+            }))
+        )    
+})
+
+// Minify and concat compiled CSS
+gulp.task('minify-css', ['copy-css'], function() {
+    return gulp.src(['css/*.css','!css/*.min.css'])
         .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('css'))
+        .pipe(concat('portfolio.min.css'))
+        .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.reload({
             stream: true
         }))
 });
 
+// Copy vendor libraries from /node_modules into
+gulp.task('concat-js',['minify-js'], function() {
+    return gulp.src([  './node_modules/jquery/dist/jquery.min.js',
+                './node_modules/bootstrap/dist/js/bootstrap.min.js',
+                './js/easing.min.js',
+                './node_modules/scrollreveal/dist/scrollreveal.min.js',
+                './js/creative.min.js',
+                './js/jqBootstrapValidation.min.js',
+                './js/contact_me.min.js'
+            ])
+    .pipe(concat('portfolio.min.js'))
+    .pipe(header(banner, { pkg: pkg }))
+    .pipe(gulp.dest('./dist/js'))
+})
+
 // Minify JS
 gulp.task('minify-js', function() {
-    return gulp.src('js/creative.js')
+    return gulp.src(['js/creative.js','js/easing.js','js/jqBootstrapValidation.js','js/contact_me.js'])
         .pipe(uglify())
-        .pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('js'))
         .pipe(browserSync.reload({
@@ -50,33 +90,8 @@ gulp.task('minify-js', function() {
         }))
 });
 
-// Copy vendor libraries from /node_modules into /vendor
-gulp.task('copy', function() {
-    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
-
-    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
-
-    gulp.src(['node_modules/magnific-popup/dist/*'])
-        .pipe(gulp.dest('vendor/magnific-popup'))
-
-    gulp.src(['node_modules/scrollreveal/dist/*.js'])
-        .pipe(gulp.dest('vendor/scrollreveal'))
-
-    gulp.src([
-            'node_modules/font-awesome/**',
-            '!node_modules/font-awesome/**/*.map',
-            '!node_modules/font-awesome/.npmignore',
-            '!node_modules/font-awesome/*.txt',
-            '!node_modules/font-awesome/*.md',
-            '!node_modules/font-awesome/*.json'
-        ])
-        .pipe(gulp.dest('vendor/font-awesome'))
-})
-
 // Run everything
-gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
+gulp.task('default', ['minify-css', 'concat-js']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
@@ -88,11 +103,10 @@ gulp.task('browserSync', function() {
 })
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
+gulp.task('dev', ['browserSync', 'minify-css', 'concat-js'], function() {
     gulp.watch('less/*.less', ['less']);
     gulp.watch('css/*.css', ['minify-css']);
-    gulp.watch('js/*.js', ['minify-js']);
+    gulp.watch('js/*.js', ['concat-js']);
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
-    gulp.watch('js/**/*.js', browserSync.reload);
 });
--- js/creative.js
+++ js/creative.js
@@ -5,7 +5,7 @@
     $(document).on('click', 'a.page-scroll', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            scrollTop: ($($anchor.attr('href')).offset().top - 30)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
@@ -31,9 +31,9 @@
     // Initialize and Configure Scroll Reveal Animation
     window.sr = ScrollReveal();
     sr.reveal('.sr-icons', {
-        duration: 600,
+        duration: 1200,
         scale: 0.3,
-        distance: '0px'
+        distance: '0px',
     }, 200);
     sr.reveal('.sr-button', {
         duration: 1000,
@@ -44,21 +44,5 @@
         scale: 0.3,
         distance: '0px'
     }, 300);
-
-    // Initialize and Configure Magnific Popup Lightbox Plugin
-    $('.popup-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        }
-    });
 
 })(jQuery); // End of use strict
